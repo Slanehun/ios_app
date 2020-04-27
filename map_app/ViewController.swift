ca6cd5a365bca1500690d29e59375e8e226877fc
Mate Devenyi <matedevenyi@gmail.com>
2020-04-27 10:13:13 +0200

    First commit / App created with WKWebview

--- map_app/ViewController.swift
+++ map_app/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.fejlesztech.hu")!
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 
 
