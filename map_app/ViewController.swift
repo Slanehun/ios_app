3c1760ba454a11755f822da96f41a18b9caec4ff
Mate Devenyi <matedevenyi@gmail.com>
2020-04-27 09:02:24 +0200

    Initial Commit

---
+++ map_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map_app
+//
+//  Created by Dévényi Máté on 2020. 04. 27..
+//  Copyright © 2020. fejleszTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
